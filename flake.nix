4b423c10de022c094d80dc397318200af8210caa
tacogips <me@tacogips.me>
2024-07-12 10:52:13 +0900

    bump nix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs =
     {
-      nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+      nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
       flake-utils.url = "github:numtide/flake-utils";
       fenix =
         {
